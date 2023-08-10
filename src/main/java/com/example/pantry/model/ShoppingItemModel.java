504655e6e908f97fd2117d9ae9aecc640990ce7a
mtryt <mat.trytek@gmail.com>
2023-08-10 23:13:45 +0200

    Done: remodel Shopping List

--- src/main/java/com/example/pantry/model/ShoppingItemModel.java
+++ src/main/java/com/example/pantry/model/ShoppingItemModel.java
@@ -1,6 +1,8 @@
 package com.example.pantry.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,21 +19,16 @@ public class ShoppingItemModel {
     @Column(name = "shopping_item_id")
     private Long shoppingItemId;
 
+    @NotEmpty(message = "Product name cannot be empty.")
     @Column(name = "name")
     private String itemName;
 
+    @Min(value = 1, message = "Quantity should not be less than 1.")
     @Column(name = "quantity")
     private Integer itemQuantity;
 
-//    @Column(name = "basket")
-//    boolean basket;
-
-//    @Column(name = "destination")
-//    private String destination;
-
-//    @ManyToOne
-//    @JoinColumn(name = "product_id")
-//    private ProductModel product;
-
+    @NotEmpty(message = "Measurement unit cannot be empty.")
+    @Column(name = "measurement_unit")
+    private String measurementUnit;
 
 }
