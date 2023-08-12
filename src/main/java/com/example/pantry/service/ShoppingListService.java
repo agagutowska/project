e452fea2894816f6e210bc710999981e66d458b1
agug <gutowska93@gmail.com>
2023-08-12 21:33:21 +0200

    update ShoppingItem classes into ShoppingList classes + added MeasurementUnitEnum into them

---
+++ src/main/java/com/example/pantry/service/ShoppingListService.java
@@ -0,0 +1,39 @@
+package com.example.pantry.service;
+
+import com.example.pantry.model.ShoppingListModel;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import com.example.pantry.repository.ShoppingListRepository;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ShoppingListService {
+
+    private final ShoppingListRepository shoppingListRepository;
+
+    public void addProduct(ShoppingListModel item) {
+        shoppingListRepository.save(item);
+    }
+
+    public List<ShoppingListModel> getItemList() {
+        return shoppingListRepository.findAll();
+    }
+
+    public ShoppingListModel findById(Long shoppingItemId) {
+        return shoppingListRepository.findById(shoppingItemId).orElse(null);
+    }
+
+    public void deleteItem(Long shoppingItemId) {
+        shoppingListRepository.deleteById(shoppingItemId);
+    }
+
+    public Page<ShoppingListModel> getAllItems(Pageable pageable) {
+        return shoppingListRepository.findAll(pageable);
+    }
+
+}
